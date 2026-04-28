66aa784c34c3f93acd1c7205e68e2f4874de43c3
Ringloo23 <ring49623@gmail.com>
2026-04-28 20:27:24 +0800

    下拉刷新更新
    --task=1031706@tapd-40058276 --user=叶庆玲 新闻资讯页下拉刷新 https://www.tapd.cn/40058276/s/1293271 --story=1031705@tapd-40058276 --user=叶庆玲 下拉刷新 https://www.tapd.cn/40058276/s/1293277

--- pages/news/news.js
+++ pages/news/news.js
@@ -5,7 +5,8 @@ Page({
   data: {
     categoryList: [],
     newsList: [],
-    currentCategory: 0
+    currentCategory: 0,
+    lastRefreshTime: ''
   },
 
   onLoad() {
@@ -17,11 +18,25 @@ Page({
     this.loadNews();
   },
 
+  onPullDownRefresh() {
+    this.loadCategories();
+    this.loadNews();
+
+    const now = new Date();
+    const h = String(now.getHours()).padStart(2, '0');
+    const m = String(now.getMinutes()).padStart(2, '0');
+    const s = String(now.getSeconds()).padStart(2, '0');
+
+    setTimeout(() => {
+      wx.stopPullDownRefresh();
+      this.setData({ lastRefreshTime: `${h}:${m}:${s}` });
+      wx.showToast({ title: '已刷新', icon: 'success', duration: 1000 });
+    }, 600);
+  },
+
   loadCategories() {
     const categories = data.getCategoryList();
-    this.setData({
-      categoryList: categories
-    });
+    this.setData({ categoryList: categories });
   },
 
   loadNews() {
@@ -31,23 +46,17 @@ Page({
       ...item,
       categoryColor: util.getCategoryColor(item.categoryId)
     }));
-    this.setData({
-      newsList: newsWithColor
-    });
+    this.setData({ newsList: newsWithColor });
   },
 
   onTabChange(e) {
     const categoryId = parseInt(e.currentTarget.dataset.id);
-    this.setData({
-      currentCategory: categoryId
-    });
+    this.setData({ currentCategory: categoryId });
     this.loadNews();
   },
 
   onNewsTap(e) {
     const id = e.currentTarget.dataset.id;
-    wx.navigateTo({
-      url: '/pages/newsDetail/newsDetail?id=' + id
-    });
+    wx.navigateTo({ url: '/pages/newsDetail/newsDetail?id=' + id });
   }
 });
