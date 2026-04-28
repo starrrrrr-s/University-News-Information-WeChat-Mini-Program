654e19e50ebd09d94bebd0d80df3262739ac05f8
Ringloo23 <ring49623@gmail.com>
2026-04-28 20:33:57 +0800

    下拉刷新优化
    --task=1031708@tapd-40058276 --user=叶庆玲 讲座页下拉刷新 https://www.tapd.cn/40058276/s/1293303 --story=1031707@tapd-40058276 --user=叶庆玲 下拉刷新 https://www.tapd.cn/40058276/s/1293305

--- pages/lecture/lecture.js
+++ pages/lecture/lecture.js
@@ -2,7 +2,8 @@ const data = require('../../utils/data.js');
 
 Page({
   data: {
-    lectureList: []
+    lectureList: [],
+    lastRefreshTime: ''
   },
 
   onLoad() {
@@ -13,17 +14,28 @@ Page({
     this.loadLectures();
   },
 
+  onPullDownRefresh() {
+    this.loadLectures();
+
+    const now = new Date();
+    const h = String(now.getHours()).padStart(2, '0');
+    const m = String(now.getMinutes()).padStart(2, '0');
+    const s = String(now.getSeconds()).padStart(2, '0');
+
+    setTimeout(() => {
+      wx.stopPullDownRefresh();
+      this.setData({ lastRefreshTime: `${h}:${m}:${s}` });
+      wx.showToast({ title: '已刷新', icon: 'success', duration: 1000 });
+    }, 600);
+  },
+
   loadLectures() {
     const lectures = data.getLectureList();
-    this.setData({
-      lectureList: lectures
-    });
+    this.setData({ lectureList: lectures });
   },
 
   onLectureTap(e) {
     const id = e.currentTarget.dataset.id;
-    wx.navigateTo({
-      url: '/pages/lectureDetail/lectureDetail?id=' + id
-    });
+    wx.navigateTo({ url: '/pages/lectureDetail/lectureDetail?id=' + id });
   }
 });
